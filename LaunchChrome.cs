b9f9c6576c0e459099ae2b41b603dd4ebc4fc97b
Justin Shafer <justinshafer@gmail.com>
2018-02-21 20:48:41 -0600

    Adding Files

---
+++ LaunchChrome.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaunchChrome
+{
+    public partial class LaunchChrome : Form
+    {
+        public class ComboboxItem
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+        public LaunchChrome()
+        {
+            InitializeComponent();
+            ComboboxItem item = new ComboboxItem();
+            item.Text = "Labels";
+            ComboboxItem item2 = new ComboboxItem();
+            item2.Text = "Texts";
+            ComboboxItem item3 = new ComboboxItem();
+            item3.Text = "Letters";
+            ComboboxItem item4 = new ComboboxItem();
+            item4.Text = "Mardi Gras Photos";
+            comboBox1.Items.Add(item);
+            comboBox1.Items.Add(item2);
+            comboBox1.Items.Add(item3);
+            comboBox1.Items.Add(item4);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            string SelectedItem = comboBox1.SelectedItem.ToString();
+            if (SelectedItem == "Labels")
+            {
+                System.Diagnostics.Process.Start("https://www.google.com/search?ei=AiyOWuKPIcWhzwLwzYagAw&q=labels&oq=labels");
+            }
+            if (SelectedItem == "Texts")
+            {
+                System.Diagnostics.Process.Start("https://www.google.com/search?ei=5CuOWqK3HYrczwKRy47YCw&q=texts&oq=texts");
+            }
+            if (SelectedItem == "Letters")
+            {
+                System.Diagnostics.Process.Start("https://www.google.com/search?source=hp&ei=4iuOWvnJBcGWzwL1566gAg&q=letters&oq=letters");
+            }
+            if (SelectedItem == "Marti Gras Photos")
+            {
+                System.Diagnostics.Process.Start("https://www.google.com/search?hl=en&tbm=isch&source=hp&biw=1920&bih=974&ei=lyuOWuXZB4-wzwKmh5iIDw&q=marti+gras+photos&oq=marti+gras+photos");
+            }
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Printing;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+using OpenDental;
+using OpenDental.UI;
+using System.IO;
+
+namespace LaunchChrome
+{//The namespace for this class must match the dll filename, including capitalization.  All other classes will typically belong to the same namespace too, but that's not a requirement.
+ /// <summary>Required class.  Don't change the name.</summary>
+    public class Plugin : PluginBase
+    {
+        private Form host;
+
+        ///<summary></summary>
+        public override Form Host
+        {
+            get
+            {
+                return host;
+            }
+            set
+            {
+                host = value;
+                ConvertPluginDatabase.Begin();//if this crashes, it will bubble up and result in the plugin not loading.
+                                              //If the plugin is only for personal use, then data tables do not need to be managed in the code.
+                                              //They could instead be managed manually using a tool.
+            }
+        }
+
+        public override bool HookMethod(object sender, string hookName, params object[] parameters)
+        {//required method
+            switch (hookName)
+            {
+                default:
+                    return false;//this plugin does not implement the particular hook passed in.
+            }
+        }
+
+        public override bool HookAddCode(object sender, string hookName, params object[] parameters)
+        {//required method
+            switch (hookName)
+            {
+
+                default:
+                    return false;
+            }
+        }
+
+        public override void LaunchToolbarButton(long patNum)
+        {
+
+            LaunchChrome frm = new LaunchChrome();
+            frm.ShowDialog();
+
+        }
+
+    }
+}
